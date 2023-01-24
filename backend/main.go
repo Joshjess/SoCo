28b54fe7b28f89b67b7bb2ee47bdab4d5fbf6c6f
Joshjess <joshuajessy47@gmail.com>
2023-01-24 17:39:25 +0100

    cors fix

--- backend/main.go
+++ backend/main.go
@@ -6,13 +6,27 @@ import (
 	PostRoutes "backend/routes/post"
 	UserRoutes "backend/routes/user"
 	"net/http"
-	"time"
 
-	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
+
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
+}
 
 func main() {
 	dsn := "host=localhost user=postgres password=password123 dbname=postgres port=5432 sslmode=disable TimeZone=Europe/Amsterdam"
@@ -24,14 +38,14 @@ func main() {
 	db.AutoMigrate(&db_schema.User{}, &db_schema.Post{}, &db_schema.Comment{}, db_schema.VotePost{}, db_schema.VoteComment{})
 
 	//seup CORS
-	myCors := cors.New(cors.Config{
-		AllowOrigins:     []string{"*"},
-		AllowMethods:     []string{"PUT", "PATCH", "GET", "POST", "DELETE"},
-		AllowHeaders:     []string{"Origin"},
-		ExposeHeaders:    []string{"Content-Length"},
-		AllowCredentials: false,
-		MaxAge:           12 * time.Hour,
-	})
+	// myCors := cors.New(cors.Config{
+	// 	AllowOrigins:     []string{"*"},
+	// 	AllowMethods:     []string{"PUT", "PATCH", "GET", "POST", "DELETE"},
+	// 	AllowHeaders:     []string{"Origin"},
+	// 	ExposeHeaders:    []string{"Content-Length"},
+	// 	AllowCredentials: false,
+	// 	MaxAge:           12 * time.Hour,
+	// })
 
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
@@ -40,7 +54,7 @@ func main() {
 		})
 	})
 	v1 := r.Group("/v1")
-	v1.Use(myCors)
+	v1.Use(CORSMiddleware())
 	{
 		UserRoutes.AddUserRoutes(v1, db)
 		PostRoutes.AddPostRoutes(v1, db)
