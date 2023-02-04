c9a51e9e6ac6b9f9851999a2e6af3ee3e27710c1
Raiduy <radu.apsan@gmail.com>
2023-02-04 16:02:10 +0100

    fix docker npm error

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,15 +3,16 @@ import Home from "./pages/Home";
 import Extra from "./pages/Extra";
 import LogInPage from "./pages/LogInPage";
 import SignUpPage from "./pages/SignUpPage";
-// require('dotenv').config()
 
 export default function App() {
   return (
     <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path="/login" element={<LogInPage />} />
-      <Route path="/signup" element={<SignUpPage />} />
-      <Route path="/extra" element={<Extra />} />
+      <Route path='/'>
+        <Route index element={<Home />} />
+        <Route path="login" element={<LogInPage />} />
+        <Route path="signup" element={<SignUpPage />} />
+        <Route path="extra" element={<Extra />} />
+      </Route>
     </Routes>
   );
 }
